693f64d39c5a9fef159dda994a6bfe11998d2e39
Scott Hain <shain@chef.io>
2018-07-06 15:20:36 -0700

    Build some scripts
    Signed-off-by: Scott Hain <shain@chef.io>

--- .buildkite/scripts/02_run_lfs_setup.sh
+++ .buildkite/scripts/02_run_lfs_setup.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
-id
-ls -la
-pwd
+cat > ~/.bash_profile << "EOF"
+exec env -i HOME=$HOME TERM=$TERM PS1='\u:\w\$ ' /bin/bash
+EOF
+cat > ~/.bashrc << "EOF"
+set +h
+umask 022
+LFS=/mnt/lfs
+LC_ALL=POSIX
+LFS_TGT=$(uname -m)-lfs-linux-gnu
+PATH=/tools/bin:/bin:/usr/bin
+export LFS LC_ALL LFS_TGT PATH
+EOF
+source ~/.bash_profile
+cd $LFS/sources
+wget http://www.linuxfromscratch.org/lfs/view/stable/wget-list
+wget --input-file=wget-list --continue --directory-prefix=$LFS/sources
+xz -d *.xz
+gunzip *.tar.gz
+bunzip2 *.bz2
+
+ls -la $LFS/sources
+echo "DOING THINGS YAY"
